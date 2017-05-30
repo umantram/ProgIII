275b2eef086275850b1903ccd2bbca59659359ad
umantram <umantram@gmail.com>
2017-05-30 20:42:27 -0300

    lo de base

---
+++ conexionBaseDatos/conexionBaseDatos/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace conexionBaseDatos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            System.Data.SqlClient.SqlConnection conn =
+                new System.Data.SqlClient.SqlConnection();
+            // TODO: Modify the connection string and include any
+            // additional required properties for your database.
+            //conn.ConnectionString =
+            // "integrated security=SSPI;data source=SQL Server Name;" +
+            // "persist security info=False;initial catalog=primeraconexion";
+
+            conn.ConnectionString =
+            "Data Source=localhost;" +
+            "Initial Catalog=primeraconexion;" +
+            "User id=umantram;" +
+            "Password=Secret;";
+
+            
+
+            try
+            {
+                conn.Open();
+                // Insert code to process data.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect to data source");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ conexionBaseDatos/conexionBaseDatos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace conexionBaseDatos
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            //ConnectToSql;
+
+        }
+
+        public static void ConnectToSql()
+        {
+            System.Data.SqlClient.SqlConnection conn =
+                new System.Data.SqlClient.SqlConnection();
+            // TODO: Modify the connection string and include any
+            // additional required properties for your database.
+            conn.ConnectionString =
+             "integrated security=SSPI;data source=SQL Server Name;" +
+             "persist security info=False;initial catalog=northwind";
+            try
+            {
+                conn.Open();
+                // Insert code to process data.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect to data source");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
